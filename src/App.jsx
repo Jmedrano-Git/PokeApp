3a9c0824d7dfbc50f1d3f8251700f4ea60b95c51
Julio Medrano <julio.medrano@tecsup.edu.pe>
2025-11-01 23:12:32 -0500

    Estructura inicial y punto de entrada principal.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/common/Navbar.jsx'
+import Footer from './components/common/Footer.jsx'
+import HomePage from './pages/HomePage.jsx'
+import ListPage from './pages/ListPage.jsx'
+import ContactPage from './pages/ContactPage.jsx'
+import NotFound from './pages/NotFound.jsx'
+
 function App() {
   return (
-    <>
-    </>
+    <div className="bg-dark text-light min-vh-100 d-flex flex-column">
+      <Navbar />
+      <main className="container py-4 flex-grow-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/lista" element={<ListPage />} />
+          <Route path="/contacto" element={<ContactPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
+
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-//Boostrap
+// Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx' // ✅ con default export y extensión JSX
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
